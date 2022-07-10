441114534a22ab41a9748010bf1129da013a9483
Christopher Pang <chrispangg@gmail.com>
2022-07-10 15:39:49 +1200

    Time: 244 ms (23.42%), Space: 15 MB (41.28%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,24 +1,27 @@
-# HashMap, where key = target-num and value = pos in array - num i.e. 7:1 for the first value in the array
-# for-loop
-    # if hashmap has key 7, then get value and put that into the first array box
-    # then put arr[i] into the second array box
+# Two Pointers
+    # if sum of two value is less than target, move left pointer up
+    # if sum of two value is more than target, move right pointer down
+    # if sum = to target return the position of array (remember to plus 1)
+# Two Pointers meet in at num[i] return empty array
+
 # Time Complexity = O(n)
 # Space Complexity = O(1)
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        hashMap = {}
         res = []
+        l = 0
+        r = len(numbers) - 1
         
-        for i, number in enumerate(numbers):
-            reminder = target - number
-            
-            if(number in hashMap):
-                res.append(hashMap.get(number))
-                res.append(i + 1)
+        for num in numbers:
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            if numbers[l] + numbers[r] == target:
+                res.append(l + 1)
+                res.append(r + 1)
                 return res
-            else:
-                hashMap[reminder] = i + 1
+            
         return res
-    
