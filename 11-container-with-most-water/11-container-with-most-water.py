2f3c78dc7676e90b232afcf6b18324f5d3296296
Christopher Pang <chrispangg@gmail.com>
2022-07-11 08:06:11 +1200

    Time: 1986 ms (5.01%), Space: 27.4 MB (54.54%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -2,7 +2,7 @@
 lines cannot be re-ordered
 2 pointers
 store area value. Area = min(two points)* distance(n[i] - n(j))
-given example: height = [1,15,6,2,5,4,13,8,12] || 8, 96, 36
+given example: height = [1,15,6,2,5,4,13,8,12]
 first loop:
 i = 0
 j = 9-1
@@ -15,12 +15,10 @@ move the pointer that's giving the smallest value
 """
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maxArea = 0
-        l = 0
-        r = len(height) - 1
+        maxArea, l, r = 0, 0, len(height) - 1
+        
         while l < r:
-            area = min(height[l], height[r]) * (r-l)
-            maxArea = max(maxArea, area)
+            maxArea = max(maxArea, min(height[l], height[r]) * (r-l))
             if(height[l] <= height[r]):
                 l += 1
             else: 
