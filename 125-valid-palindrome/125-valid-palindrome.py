fb420b09a037021b84328d33e19881700c6f26d5
Christopher Pang <chrispangg@gmail.com>
2022-07-10 14:31:50 +1200

    Time: 55 ms (81.90%), Space: 14.4 MB (57.62%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,16 @@
 # 1. remove space and comma, fullstop
+# 2. two pointers - compare the first and last
+# beware that the -1 is the last digit, but the first digit is 0
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = ''.join(e for e in s if e.isalnum()).lower()
+        string = ""
+        for c in s:
+            if c.isalnum():
+                string = string + c.lower()
+        
+        #Alternative: string = ''.join(e for e in s if e.isalnum()).lower()
+        
         for i, ele in enumerate (string):
             if string[i] != string[(i * -1) - 1]:
-                    return False
-                
+                    return False       
         return True
