2b3f3d6d8d50fed8ba89b35fee07999d340d3f5c
Christopher Pang <chrispangg@gmail.com>
2022-07-12 18:03:27 +1200

    Time: 47 ms (53.92%), Space: 13.8 MB (70.03%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,30 @@
+# Use a map for the parentheses
+# Use a stack to keep track of what's on top
+# If can't get the given parenthese does not match the value of the map, then return false
+# If map is empty, also return false
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0 or len(s) == 1 : 
+            return False
+        
+        p = {"{" : "}", "[" : "]", "(" : ")"}
+        
+        stack = []
+        
+        for i in range(len(s)):
+            if s[i] in p:
+                stack.append(s[i])
+            
+            else:
+                if len(stack) == 0: 
+                    return False
+                
+                val = stack.pop()
+                if p[val] != s[i]:
+                    return False
+        
+        if len(stack) != 0:
+            return False
+        
+        return True
