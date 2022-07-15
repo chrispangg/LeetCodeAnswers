3ad51951576385f2e1da89cba00acaa6942d8fc8
Chris Pang <chrispangg@gmail.com>
2022-07-15 15:47:50 +1200

    Time: 2371 ms (69.82%), Space: 30.1 MB (37.30%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -0,0 +1,36 @@
+"""
+Use a queue:
+    1. check if nums[r] is greater than the top value of the queue (queue[-1]). If it is we queue.pop() until nums[r] is no longer greater than queue[-1]
+    1. add nums[r] to the queue to the right side
+    2. remove nums[l] from the left of the queue if num[l] is at queue[0] or if value 
+    3. remove value less than or equal to the lesftest value in the queue (using a while loop) and len(queue) != 1
+    4. if r+1 - l == k, add leftest value from queue to res
+
+"""
+
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = deque([])
+        res = []
+        l = 0
+        
+        for r in range (len(nums)):
+            #pop smaller values from queue
+            while queue and nums[r] > nums[queue[-1]]:
+                queue.pop()
+            
+            #append value to the right of queue
+            queue.append(r)    
+            
+            #remove left val from window
+            if l > queue[0]:
+                queue.popleft()
+                
+            if (r+1) >= k:
+                res.append(nums[queue[0]]) 
+                l += 1
+        
+        return res
+        
