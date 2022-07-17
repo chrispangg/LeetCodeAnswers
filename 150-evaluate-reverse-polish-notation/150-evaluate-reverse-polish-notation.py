3495fe81b1ba34211fd96dbd98221b34c9b44e23
Chris Pang <chrispangg@gmail.com>
2022-07-17 14:57:39 +1200

    Time: 80 ms (78.15%), Space: 14.4 MB (56.91%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,17 @@
-"""
-Append to Stack for every number.
-When an operator appears, pop the last two number to perform operations
-Then, replace tokens[i-2] with the new number returned, also save the new number in a variable
-return the variable at the end of the loop
-
-"""
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for i, num in enumerate(tokens):
-            stack.append(num)
-            
-            if num == "+" or num == "-" or num == "*" or num == "/":
-                # print(str(stack))
-                symbol = stack.pop()
-                top = stack.pop()
-                lastTop = stack.pop()
-                res = eval(str(lastTop + symbol + top))
-                
-                stack.append(str(int(res)))
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
         return stack[0]
