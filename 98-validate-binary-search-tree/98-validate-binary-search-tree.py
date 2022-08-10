0c875f4d04fc810348b32586bd972983a4730af1
Chris Pang <chrispangg@gmail.com>
2022-08-10 15:08:18 +1200

    Time: 42 ms (97.46%), Space: 17.1 MB (14.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -15,12 +15,7 @@ class Solution:
             return arr
         
         arr = dfs(root, [])
-        print(arr)
         for i in range(1, len(arr)):
             if arr[i] <= arr[i-1]:
                 return False
         return True
-        
-            
-            
-            
