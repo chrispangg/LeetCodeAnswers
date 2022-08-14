a1d1e84b9cb52d6f469d507da1d08584f25b9126
Chris Pang <chrispangg@gmail.com>
2022-08-15 09:34:36 +1200

    Time: 1300 ms (68.02%), Space: 32.8 MB (94.62%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,15 +1,17 @@
+# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3] output = 3
+# use a monotonic increasing stack
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p,s] for p,s in zip(position, speed)] #create an array of [pos, speed] pairs
+        pairs = list(zip(position, speed))
         
         stack = []
-        for p, s in sorted(pair)[::-1]: #Reverse Sorted Order
+        for p, s in sorted(pairs)[::-1]:
             time = (target - p) / s
             stack.append(time)
-            #if the car we appended stack[-1] will take less time than the car before stack[-2],
-            # then we know it will catch up, and thus it will merge with stack[-1].
-            # So we can remove it from top of stack (as it is now merged with stack[-1])
-            if len(stack) >= 2 and stack[-1] <= stack[-2]: 
+            print(time)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
-        
+                
         return len(stack)
+            
