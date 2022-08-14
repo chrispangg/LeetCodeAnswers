fc2df33e60d014c365571794d03d75118e7c924a
Chris Pang <chrispangg@gmail.com>
2022-08-15 09:46:02 +1200

    Time: 928 ms (98.06%), Space: 32.1 MB (97.18%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,16 +1,9 @@
-# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3] output = 3
-# use a monotonic increasing stack
-
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pairs = list(zip(position, speed))
-        
-        stack = []
+        arr = []
         for p, s in sorted(pairs)[::-1]:
             time = (target - p) / s
-                
-            if len(stack) < 1 or len(stack) >= 1 and time > stack[-1]:
-                stack.append(time)
-                
-        return len(stack)
-            
+            if len(arr) < 1 or len(arr) >= 1 and time > arr[-1]:
+                arr.append(time)
+        return len(arr)
