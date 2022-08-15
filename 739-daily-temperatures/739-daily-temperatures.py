cb0fab29a188cc4dfb1f45398098b09eb7fbccae
Chris Pang <chrispangg@gmail.com>
2022-08-15 21:20:17 +1200

    Time: 2177 ms (19.13%), Space: 23.9 MB (98.94%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,17 +1,17 @@
 """
-Use a decreasing stack to keep track of the lowest temp
-Pop the stack when it encounter a higher value
-
-Time: O(N)
+Monotonic decreasing stack, where it stores both the temperature and index
+- Use the index for caculating the differences between the current position and the position in the stack to find temperature diff
+- Use the index for placing our result in the correction position in the result array
+temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
 """
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = [] #pair: [temp, index], add index for calculating the num of days to find a higher temp
-        
+        stack = []
         for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]: #stack[-1][0] gets the temp
+            while stack and stack[-1][0] < t:
                 stackT, stackInd = stack.pop()
-                res[stackInd] = i - stackInd #diff between current temp and the next lowest temp
+                res[stackInd] = i - stackInd
             stack.append([t,i])
         return res
