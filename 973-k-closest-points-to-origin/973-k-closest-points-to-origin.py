3cd636e67fbceb1bf3c6a921f44dfdce21e5abb0
Chris Pang <chrispangg@gmail.com>
2022-08-22 10:43:00 +1200

    Time: 926 ms (81.61%), Space: 20.5 MB (21.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -7,7 +7,6 @@ distance = sqrt(x^2 + y^2)
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         distances = []
-        heapq.heapify(distances)
         for x, y in points:
             distance = -(x**2 + y**2)
             if len(distances) == k:
