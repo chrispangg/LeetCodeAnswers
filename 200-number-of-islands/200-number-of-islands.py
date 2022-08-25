2644dabe63ba6aa3ccfe25e1a11bf25aec953b3b
Chris Pang <chrispangg@gmail.com>
2022-08-25 14:11:27 +1200

    Time: 372 ms (80.32%), Space: 16.5 MB (49.88%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,28 @@
+"""
+Iterate through each of the cell and if it is an island, do dfs to mark all adjacent islands, then increase the counter by 1.
+"""
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(r , c):
+            if (r < 0 or c < 0 or 
+                r >= len(grid) or c >= len(grid[0]) or 
+                grid[r][c] != '1'):
+                return
+            
+            grid[r][c] = '#'
+            
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+            
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == '1':
+                    dfs(r, c)
+                    count += 1
+        
+        return count
+                       
