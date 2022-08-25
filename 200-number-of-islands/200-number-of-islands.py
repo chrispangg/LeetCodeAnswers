fc7e6dd3780910179ded1532ba4466176efd3531
Chris Pang <chrispangg@gmail.com>
2022-08-25 20:40:36 +1200

    Time: 505 ms (46.39%), Space: 21.9 MB (27.04%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,29 +1,35 @@
-"""
-Iterate through each of the cell and if it is an island, do dfs to mark all adjacent islands, then increase the counter by 1.
-"""
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
         
-        def dfs(r , c):
-            if (r < 0 or c < 0 or 
-                r >= len(grid) or c >= len(grid[0]) or 
-                grid[r][c] != '1'):
-                return
-            
-            grid[r][c] = '#'
-            
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-            
-        count = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == '1':
-                    dfs(r, c)
-                    count += 1
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
         
-        return count
+        def bfs(r,c):
+            q = collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1,0], [0,1], [0,-1]]
+                
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if ((r) in range(rows) and
+                        (c) in range(cols) and
+                        grid[r][c] == "1" and
+                        (r, c) not in visit):
+                        q.append((r, c))
+                        visit.add((r, c))
+                
+                
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands += 1
+        return islands
