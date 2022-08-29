2585dcb2ed6789d48b0e7bc6dd1d5323c2b2eaac
Chris Pang <chrispangg@gmail.com>
2022-08-29 20:42:12 +1200

    Time: 112 ms (84.85%), Space: 17.5 MB (11.45%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,33 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        # map each course to prereq list
-        preMap = {i: [] for i in range(numCourses)}
+        preMap = {i:[] for i in range(numCourses)}
         for c, p in prerequisites:
             preMap[c] = preMap.get(c, [])
             preMap[c].append(p)
         
-        #visit Set = all courses along the curr DFS path
-        visit = set()
+        visit, cycle = set(), set()
         
         def dfs(c):
-            # base case 1: when there's a loop
-            if c in visit:
-                return False
-            # base case 2: when the course has no prereq. Return True as it can be completed
-            if preMap[c] == []:
-                return True
-            visit.add(c)
+            if c in cycle: return False
+            if c in visit: return True
+            cycle.add(c)
+            
             for p in preMap[c]:
-                if not dfs(p): # return false when there's a prereq that caused a loop
-                    return False
-            visit.remove(c) # clean up
-            preMap[c] = [] # this helps us reduce rework when we reach the same course again
+                if not dfs(p): return False
+            cycle.remove(c)
+            visit.add(c)
             return True
         
         for c in range(numCourses):
-            if not dfs(c):
-                return False
-        return True
+            if not dfs(c): return False
             
+        return True
+                
+            
+            
+            
+            
+        
         
