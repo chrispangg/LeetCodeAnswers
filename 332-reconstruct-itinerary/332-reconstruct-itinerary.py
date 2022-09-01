7c08e7e9490973db0ba1e42b4b5cac227b0216a4
Chris Pang <chrispangg@gmail.com>
2022-09-01 20:25:19 +1200

    Time: 117 ms (65.02%), Space: 15 MB (7.41%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -2,24 +2,27 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         # use deque because we want to popleft
         tickets.sort()
-        adj = {u: collections.deque() for u, v in tickets}
-        for u, v in tickets: adj[u].append(v)
+        adj = {src: collections.deque() for src, dst in tickets}
+        for src, dst in tickets: 
+            adj[src].append(dst)
+            
         res = ["JFK"]
-        
         def dfs(curr):
             if len(res) == len(tickets) + 1:
                 return True
             if curr not in adj:
                 return False
             
+            # use a copy of adj[curr] so we can actually alter the list
+            # as we loop
             for v in list(adj[curr]):
                 #remove from adj[curr] and add to v to res
                 adj[curr].popleft()
                 res.append(v)
-                if dfs(v): 
-                    return res
+                
+                if dfs(v): return res
                 #if dfs didn't go well, 
-                # pop v out from res and then add it back to adj[curr]
+                # pop v out from res and add it back to adj[curr]
                 res.pop() 
                 adj[curr].append(v)
             return False
