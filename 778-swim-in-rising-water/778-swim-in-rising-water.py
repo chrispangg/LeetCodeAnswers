e6816df2d8fac8925c45e3e907ce743d592cc61b
Chris Pang <chrispangg@gmail.com>
2022-09-03 18:04:36 +1200

    Time: 265 ms (21.37%), Space: 14.8 MB (14.23%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -0,0 +1,34 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # 1. create adjacency list, node:[neighbour nodes]
+        adj = defaultdict(list)
+        rows, cols = len(grid), len(grid[0])
+        for r in range(rows):
+            for c in range(cols):
+                node = grid[r][c]                   
+                if c + 1 < cols: adj[node].append(grid[r][c + 1])
+                if c - 1 >= 0: adj[node].append(grid[r][c - 1])
+                if r + 1 < rows: adj[node].append(grid[r + 1][c])
+                if r - 1 >= 0: adj[node].append(grid[r - 1][c]) 
+        print(adj)
+        
+        # 2. Perform Dijkstra's Algorithm
+        visited = set()
+        minHeap = [[grid[0][0], grid[0][0]]]
+        target = grid[rows - 1][cols - 1]
+        while minHeap:
+            totalcost, node = heapq.heappop(minHeap)
+                
+            if node == target: 
+                return totalcost
+            
+            if node in visited: continue
+            
+            visited.add(node)
+            
+            for nei in adj[node]:
+                if nei > totalcost: 
+                    heapq.heappush(minHeap, [nei, nei])
+                else:
+                    heapq.heappush(minHeap, [totalcost, nei])
+        
