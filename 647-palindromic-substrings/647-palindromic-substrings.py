ae415451a35f69a6aba22164f12df0f2a8c20d5d
Chris Pang <chrispangg@gmail.com>
2022-09-09 08:13:24 +1200

    Time: 272 ms (36.99%), Space: 13.9 MB (38.69%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -3,14 +3,14 @@ class Solution:
         res = 0
         
         def findSubstring(l, r):
-            subRes = []
+            subRes = 0
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                subRes.append(s[l:r + 1])
+                subRes += 1
                 l -= 1
                 r += 1
             return subRes
         
         for i in range(len(s)):
-            res += len(findSubstring(i,i)) + len(findSubstring(i,i+1))
+            res += findSubstring(i,i) + findSubstring(i,i+1)
             
         return res
