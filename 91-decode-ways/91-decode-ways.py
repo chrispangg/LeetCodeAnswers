2bcc354e9380f3a2edf00ce96e82adf84d02a351
Chris Pang <chrispangg@gmail.com>
2022-09-09 16:09:44 +1200

    Time: 71 ms (10.41%), Space: 14 MB (46.41%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,21 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        cache = {len(s):1} #if empty string return 1
-        def dfs(i):
-            # base case 1: if cached or reached end of string 
-            if i in cache:
-                return cache[i]
+				# Dynamic Programming
+        dp = {len(s): 1}
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
-                return 0
-            
-            res = dfs(i + 1)
-            
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+
             if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
-                res += dfs(i + 2)
-            
-            cache[i] = res
-            
-            return res
-        
-        return dfs(0)
-        
+                dp[i] += dp[i + 2]
+        return dp[0]
