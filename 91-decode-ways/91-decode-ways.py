d8eb432b1af286f3f616adf4c63a2572ce8c36d5
Chris Pang <chrispangg@gmail.com>
2022-09-09 16:17:16 +1200

    Time: 72 ms (9.40%), Space: 14.1 MB (19.42%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -4,15 +4,17 @@ class Solution:
         def dfs(i):
             # base case 1: if cached or reached end of string 
             if i in cache:
+                print("it's in cache: " + str(i))
                 return cache[i]
-            if s[i] == "0":
-                return 0
+            # base case 2: if we see a 0, it means branch is invalid
+            if s[i] == "0": return 0
             
             res = dfs(i + 1)
             
             if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
                 res += dfs(i + 2)
             
+            print("i: " + str(i) + " res: " + str(res))
             cache[i] = res
             
             return res
