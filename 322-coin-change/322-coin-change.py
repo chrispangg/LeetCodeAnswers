f821fec2520d45d19c59a77d4bc4b6d782bd8290
Chris Pang <chrispangg@gmail.com>
2022-09-10 16:34:55 +1200

    Time: 1600 ms (84.19%), Space: 14.2 MB (86.22%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,9 +3,13 @@ class Solution:
         cache = [float('inf')] * (amount + 1) # 0...amount
         cache[0] = 0
         
-        for a in range(1, amount + 1):
+        for a in range(amount + 1):
             for c in coins:
                 if a - c >= 0:
+                    """
+                    coin = 4, a = 7
+                    cache[7] = 1 + cache[7-4]
+                    """
                     cache[a] = min(cache[a], 1 + cache[a - c])
         
         return cache[amount] if cache[amount] != float('inf') else -1
