822c62ca6b5f68291428accdc5f5ad73cb25af77
Chris Pang <chrispangg@gmail.com>
2022-09-10 16:30:36 +1200

    Time: 3860 ms (9.73%), Space: 14.4 MB (41.83%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,21 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        level = [0]
-        num = 0
-        visited = set()
-        while level:
-            if amount in level:
-                return num
-            
-            queue = []
-            for a in level:
-                for c in coins:
-                    if a + c <= amount and a + c not in visited:
-                        queue.append(a+c)
-                        visited.add(a+c)
-                        
-            level = queue
-            num +=1
+        cache = [float('inf')] * (amount + 1) # 0...amount
+        cache[0] = 0
         
-        return -1
-            
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    cache[a] = min(cache[a], 1 + cache[a - c])
+        
+        return cache[amount] if cache[amount] != float('inf') else -1
