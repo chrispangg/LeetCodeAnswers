d4c107915f615f9b5666d9d76758b2fc3ae6797a
Chris Pang <chrispangg@gmail.com>
2022-09-12 07:54:51 +1200

    Time: 471 ms (5.05%), Space: 14.2 MB (25.84%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,16 +2,19 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         wordSet = set(wordDict)
-
+        
         @lru_cache(None)
-        def dp(start):
-            if start == n:  # Found a valid way to break words
+        def dfs(l):
+            if l == n:
                 return True
-
-            for end in range(start + 1, n + 1):  # O(N^2)
-                word = s[start:end]  # O(N)
-                if word in wordSet and dp(end):
-                    return True
-            return False
-
-        return dp(0)
+            for word in wordDict:
+                r = len(word)
+                while r <= len(s):
+                    if s[l:r] == word and dfs(r):
+                        # print(s[l:r])
+                        return True
+                    r+=1
+            
+            return False    
+        return dfs(0)
+    
