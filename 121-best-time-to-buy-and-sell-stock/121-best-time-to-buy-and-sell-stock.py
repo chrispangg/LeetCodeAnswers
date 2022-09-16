c6c43503e2703769f40e01019e70b00df94d3062
Chris Pang <chrispangg@gmail.com>
2022-09-17 09:39:33 +1200

    Time: 1063 ms (94.83%), Space: 25.1 MB (38.23%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,18 @@
 """
-Sliding Window Question: Key is we always want to start again from the lowest known number
-    - So move left pointer to the smallest known number
+Sliding Window technique
+Look for the next smallest value, while keeping track our max profit
+
 """
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res, l = 0, 0
-        
+        l,r = 0,0
+        res = 0
         for r in range(len(prices)):
-            if prices[r] - prices[l] > res:
-                res = max(res, prices[r] - prices[l])
+            res = max(res, prices[r] - prices[l])
+            
             if prices[r] < prices[l]:
                 l = r
-        
         return res
+        
+            
