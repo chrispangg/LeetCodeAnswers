c72d2bbec5e25dae1f6fbc40df8b02d8f704e922
Chris Pang <chrispangg@gmail.com>
2022-09-19 10:06:23 +1200

    Time: 48 ms (87.73%), Space: 14.9 MB (38.63%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,21 +1,21 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        if len(s1) + len(s2) != len(s3): return False
+
+        dp = {}
+        # k = i + j
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            
+            dp[(i, j)] = False
             return False
-        
-        row, col = len(s1), len(s2)
-        dp = [[False]* (col + 1) for i in range(row + 1)]
-        dp[row][col] = True 
-        
-        
-        #each element on each row (so col first)
-        for r in range(row, -1, -1):
-            for c in range(col, -1, -1):
-                #look right one cell and look left one cell
-                if r < len(s1) and s1[r] == s3[r + c] and dp[r + 1][c]:
-                    dp[r][c] = True
-                if c < len(s2) and s2[c] == s3[r + c] and dp[r][c + 1]:
-                    dp[r][c] = True
-        
-        return dp[0][0]
-                
+            
+        return dfs(0, 0)
