5930f980499be83df07f7ca94f3b16e9615e1678
Chris Pang <chrispangg@gmail.com>
2022-09-18 13:53:54 +1200

    Time: 1236 ms (6.83%), Space: 36 MB (15.46%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -6,7 +6,7 @@ class Solution:
             if i == len(nums):
                 return 1 if total == target else 0
 
-            if (i, total) in cache: 
+            if (abs(i), total) in cache: 
                 return cache[(i, total)]
             
             cache[(i, total)] = dfs(i+1, (total + nums[i])) + dfs(i+1, (total - nums[i]))
