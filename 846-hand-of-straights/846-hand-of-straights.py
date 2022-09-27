e1b04de5dfdbace5ddad019b22c1b3a3b6157820
Chris Pang <chrispangg@gmail.com>
2022-09-27 16:00:45 +1300

    Time: 400 ms (45.76%), Space: 15.9 MB (12.74%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.py
+++ 846-hand-of-straights/846-hand-of-straights.py
@@ -1,21 +1,24 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand) % groupSize:
-            return False
-
-        count = {}
-        for n in hand:
-            count[n] = 1 + count.get(n, 0)
-
-        minH = list(count.keys())
-        heapq.heapify(minH)
+        if len(hand) % groupSize != 0: return False
+        
+        dic = {}
+        for k in hand:
+            dic[k] = dic.get(k, 0) + 1
+        
+        minH = list(dic.keys())
+        heapify(minH)
+        
         while minH:
             first = minH[0]
+            
             for i in range(first, first + groupSize):
-                if i not in count:
+                if i not in dic: 
                     return False
-                count[i] -= 1
-                if count[i] == 0:
+                dic[i] -= 1
+                if dic[i] == 0:
+                    # check if the value we want to pop if the min value,
+                    # if not, then we 
                     if i != minH[0]:
                         return False
                     heapq.heappop(minH)
