35ff5ac8210146348846f12519c5f81e23bbec42
Chris Pang <chrispangg@gmail.com>
2022-10-03 08:01:46 +1300

    Time: 25 ms (99.06%), Space: 13.9 MB (33.72%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,36 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        l, r = 0, len(matrix[0]) - 1
-        t, b = 0, len(matrix) - 1
+        l, r = 0, len(matrix[0])
+        t, b = 0, len(matrix)
         while l < r and t < b:
             #from top left to top right
-            for i in range(r - l):
-                res.append(matrix[t][l + i])
-            
+            for i in range(l, r):
+                res.append(matrix[t][i])
+            t += 1
             #from top right to bottom right
-            for i in range(b - t):
-                res.append(matrix[t + i][r])
+            for i in range(t, b):
+                res.append(matrix[i][r - 1])
+            r -= 1
             
+            if not (l < r and t < b):
+                break
             
             #from bottom right to bottom left 
-            for i in range(r - l):
-                res.append(matrix[b][r - i])
-            
+            for i in range(r - 1, l - 1, -1):
+                res.append(matrix[b - 1][i])
+            b -= 1
             
             #from bottom left to top right 
-            for i in range(b - t):
-                res.append(matrix[b - i][l])
+            for i in range(b - 1, t - 1, -1):
+                res.append(matrix[i][l])
+            l += 1            
+            
         
-            #all pointers move inwards
-            l += 1
-            r -= 1
-            t += 1
-            b -= 1
-        
-        #edge case: when the last/inside is either a single row/col or cell, perform linear row scan to get remaining cells
-        if len(res) < len(matrix[0]) * len(matrix):
-            for i in range(t, b + 1):
-                for j in range(l, r + 1):
-                    res.append(matrix[i][j])
         return res
