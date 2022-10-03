038fb4fb69bba6b5006478d54c57c285d9a6f206
Chris Pang <chrispangg@gmail.com>
2022-10-03 13:58:51 +1300

    Time: 334 ms (13.70%), Space: 14.8 MB (16.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,21 +1,33 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        zeros = set()
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
+        
+        # determine which rows/cols need to be zero
+        for r in range(rows):
+            for c in range(cols):
                 if matrix[r][c] == 0:
-                    zeros.add((r, c))
+                    # set row or column (or rowZero) to zero
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+        # zero out body of the matrix                
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
         
-        # print(zeros) #row, cols {(0, 3), (0, 0)}
-        for row, col in zeros:
-            #update all values in the row
-            for r in range(len(matrix)):
-                matrix[r][col] = 0
+        # zero out the column header
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+        
+        # zero out the row header
+        if rowZero == True:
+            for c in range(cols):
+                matrix[0][c] = 0
                 
-            for c in range(len(matrix[0])):
-                matrix[row][c] = 0    
-        
-        
+                    
+                
