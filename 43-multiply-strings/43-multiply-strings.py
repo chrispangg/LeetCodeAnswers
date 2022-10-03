734e325fd39aac2c72bff55e9f5322a1a57c8c65
Chris Pang <chrispangg@gmail.com>
2022-10-03 15:50:41 +1300

    Time: 140 ms (47.03%), Space: 13.9 MB (72.86%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,3 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        res = 0
+        n_multiply, m_multiply = 1, 1
+        
+        for m in num2[::-1]: #45
+            for n in num1[::-1]: #123
+                res += int(m) * int(n) * n_multiply * m_multiply
+                n_multiply = n_multiply * 10
+            
+            n_multiply = 1
+            m_multiply = m_multiply * 10
+            
+        return str(res)
+            
+                
+                
+                
