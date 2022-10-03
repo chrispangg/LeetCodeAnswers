46d44be4012e28ff2ed4edcd8baf07426ff0572b
Chris Pang <chrispangg@gmail.com>
2022-10-03 14:43:17 +1300

    Time: 61 ms (26.93%), Space: 13.8 MB (97.62%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,3 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+        
+        def power(y, m):
+            if y == 0: return 0
+            if m == 0: return 1
+            
+            res = power(y * y, m//2)
+
+            return y * res if m % 2 == 1 else res
+
+        
+        return power(x, abs(n)) if n >= 0 else 1/power(x, abs(n))
+        
