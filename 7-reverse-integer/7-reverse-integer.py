f24b6fead2fbb5ba2fbff7903a576ab8e9a56b20
Chris Pang <chrispangg@gmail.com>
2022-10-09 20:41:06 +1300

    Time: 64 ms (33.34%), Space: 13.8 MB (64.19%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -2,9 +2,5 @@ class Solution:
     def reverse(self, x: int) -> int:
         xStr = str(abs(x))
         reversedR = int(str(xStr[::-1]))
-        if reversedR > 2147483647:
-            return 0
-        if x < 0:
-            reversedR = reversedR * -1
-        
-        return reversedR
+        if reversedR > 2147483647: return 0
+        return reversedR * -1 if x < 0 else reversedR
