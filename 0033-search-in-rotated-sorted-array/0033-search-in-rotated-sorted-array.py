e3a06bb5ef2de8cdf18398b91d2c505610199316
Chris Pang <chrispangg@gmail.com>
2022-10-16 14:09:36 +1300

    Time: 86 ms (33.14%), Space: 14.4 MB (18.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -8,12 +8,12 @@ class Solution:
             if nums[m] == target:
                 return m
             elif nums[l] <= nums[m]: #left side in order
-                if nums[l] <= target < nums[m]: #within the left side
+                if nums[l] <= target <= nums[m]: #within the left side
                     r = m - 1
                 else:
                     l = m + 1
             elif nums[m] <= nums[r]: #right side in order
-                if nums[m] < target <= nums[r]: # in the rigth side
+                if nums[m] <= target <= nums[r]: # in the rigth side
                     l = m + 1
                 else:
                     r = m - 1
