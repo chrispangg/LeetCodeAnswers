1da79112cf855b95b7c3c501c51b6ef8196b787c
Chris Pang <chrispangg@gmail.com>
2022-10-17 16:27:10 +1300

    Time: 51 ms (65.02%), Space: 13.8 MB (95.55%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,15 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        dic = {i: 0 for i in list(string.ascii_lowercase)}
-        
-        for c in sentence:
-            if c.lower() in dic:
-                dic[c] = dic[c] + 1
-        for i in dic:
-            if dic[i] < 1:
-                return False
-            
-        return True
-        
-        
-        
+        return len(set(sentence)) == 26
