37ca4f05411a7c3df26b7285178f4a6d63c93e5a
Chris Pang <chrispangg@gmail.com>
2022-10-18 21:47:24 +1300

    Time: 5558 ms (5.92%), Space: 34.3 MB (18.56%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,17 +1,29 @@
+# Solution: QuickSelect
+# Time Complexity: 
+#   - Best Case: O(n)
+#   - Average Case: O(n)
+#   - Worst Case: O(n^2)
+# Extra Space Complexity: O(1)
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k #index we are looking for after sorted
         
-        def quicksort(nums):
-            if len(nums) <= 1:
-                return nums
+        def quickSelect(l, r):
+            # partition
+            p, pivot = l, nums[r]
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+                    
+            nums[p], nums[r] = nums[r], nums[p]
             
-            pivot = nums[0]
-            less = [i for i in nums[1:] if i <= pivot]
-            greater = [i for i in nums[1:] if i > pivot]
-            
-            return quicksort(less) + [pivot] + quicksort(greater)
-        
-        arr = quicksort(nums)
-        return arr[-k]
+            if p > k: # meaning k is in left side
+                return quickSelect(l, p - 1)
+            if p < k: # meaning k is in the right side
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
             
             
+        return quickSelect(0, len(nums) - 1)
